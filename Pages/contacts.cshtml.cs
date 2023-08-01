cecb4deb89a16d6c4c5cb0b9eadfddedc1f51b17
rakesh.ardanuy <rakesh.gangwar@ardanuy.com>
2023-08-01 17:40:31 +0530

    update

--- Pages/contacts.cshtml.cs
+++ Pages/contacts.cshtml.cs
@@ -8,5 +8,62 @@ namespace RES.Web.Pages
         public void OnGet()
         {
         }
+
+
+        public void OnPost()
+        {
+            //var name = FullName;
+            //var email = Email;
+            //var mobile = ContactNumbers;
+            // now you have all submitted values in local variables
+
+
+            //var name = Request.Form["FullName"];
+            //var email = Request.Form["Email"];
+            //var mobile = Request.Form["ContactNumbers"];
+            // now you have all submitted values in local variables
+        }
+
+
+
+        public void OnPostRegisterAsync()
+        {
+            var name = Request.Form["FullName"];
+            // now you have all submitted values in local variables
+        }
+
+
+        public void OnPostRegister()
+        {
+            var name = Request.Form["FullName"];
+            // now you have all submitted values in local variables
+        }
+        public void OnPostRequest()
+        {
+            var name = Request.Form["FullName"];
+            // now you have all submitted values in local variables
+        }
+
+
+        public void OnPostAspnet(int valuecount)
+        {
+            ViewData["ActionMessage"] = $"Aspnet {valuecount}";
+        }
+        public void OnPostAspnetmvc(int valuecount)
+        {
+            ViewData["ActionMessage"] = $"AspnetMVC {valuecount}";
+        }
+        public void OnPostAspnetcore(int valuecount)
+        {
+            ViewData["ActionMessage"] = $"AspnetCore {valuecount}";
+        }
+        public void OnPostLinq(int valuecount)
+        {
+            ViewData["ActionMessage"] = $"LINQ {valuecount}";
+        }
+        public void OnPostHtml(int valuecount)
+        {
+            ViewData["ActionMessage"] = $"Html {valuecount}";
+        }
     }
 }
