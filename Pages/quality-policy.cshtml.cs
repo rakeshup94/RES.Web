a9175bbf0757c1fec23f15d07e3af5fc96d51f7e
Pardeep Kamboj <pardeep.kamboj@ardanuy.com>
2023-08-04 16:28:33 +0530

    updt

---
+++ Pages/quality-policy.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RES.Web.Pages
+{
+    public class quality_policyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
