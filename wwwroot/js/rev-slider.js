2e75aebe323dabb86117a895874e9cd5bced388c
Pardeep Kamboj <pardeep.kamboj@ardanuy.com>
2023-09-11 15:56:17 +0530

    updt

--- wwwroot/js/rev-slider.js
+++ wwwroot/js/rev-slider.js
@@ -170,7 +170,7 @@ var RevSlider = function() {
                     },
                     responsiveLevels:[2220,1183,975,750],
                     gridwidth:[1770,970,760,320],
-                    gridheight: [500,500,300,300],
+                    gridheight: [700,700,300,300],
                     lazyType: "smart",
                     parallax: {
                         type: "scroll",
